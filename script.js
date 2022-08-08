731da85ab830c5d646443de9ed285b96b2c07d6b
ELODIE TIHANGE <109513258+Tihangelodie@users.noreply.github.com>
2022-08-08 17:26:18 +0200

    Added all files

---
+++ script.js
@@ -0,0 +1,16 @@
+function displayMenu() {
+    // var menu = document.getElementById("topnav_responsive_menu");
+    // var icon = document.getElementById("topnav_hamburger_icon");
+    // if (menu.className === "") {
+    //     menu.className = "open";
+    //     icon.className = "open";
+    // } else {
+    //     menu.className = "";                    
+    //     icon.className = "";
+    // }
+
+
+    let element =document.getElementById ("sidebar-menu");
+    element.style.display="block";
+//     Not functioning yet
+// }
